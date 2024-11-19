940192036b07ee1cbb3532009c2bde706771f726
Ashish <ashish74624@gmail.com>
2024-11-19 23:20:34 +0530

    Server added

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,18 +1,50 @@
-import { useEffect } from 'react';
-import Cookies from 'js-cookie';
+import { useState, useEffect, FormEvent } from 'react';
+import io from 'socket.io-client';
 
-const Home = () => {
+const socket = io('http://localhost:3001');
+
+interface Message {
+  name: string;
+  message: string;
+}
+
+
+function Home() {
+  const [name, setName] = useState('');
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState<Message[]>([]);
+
   useEffect(() => {
-    
-    const tokenCookie = Cookies.get('token');
-    console.log('Token from cookie:', tokenCookie);
+    socket.on('message', (message) => {
+      setMessages((messages) => [...messages, message]);
+    });
   }, []);
 
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (name && message) {
+      socket.emit('sendMessage', { name, message });
+      setName('');
+      setMessage('');
+    }
+  };
+
   return (
-    <section className='bg-[#F2F2F2] h-screen w-screen'>
-      Home
-    </section>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={name} placeholder="Your name" onChange={(event) => setName(event.target.value)} />
+        <input type="text" value={message} placeholder="Your message" onChange={(event) => setMessage(event.target.value)} />
+        <button type="submit">Send</button>
+      </form>
+      <ul>
+        {messages.map((message, index) => (
+          <li key={index}>
+            {message.name}: {message.message}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
-};
+}
 
 export default Home;
