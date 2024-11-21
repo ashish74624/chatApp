7423d804d3d86aa6fadc25ad8168d2c3c3af8959
Ashish <ashish74624@gmail.com>
2024-11-21 22:19:15 +0530

    register input

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -3,7 +3,7 @@ import { z } from 'zod'
 import { Link, useNavigate } from "react-router-dom";
 import { zodResolver } from '@hookform/resolvers/zod';
 import AuthPageSVG from '@/components/AuthPageSVG';
-
+import Input from '@/components/AuthPage/Input';
 const backend = import.meta.env.VITE_BACKEND
 
 const schema = z.object({
@@ -52,24 +52,24 @@ export default function Register() {
             <h2 className=" font-GraphikBlack font-medium text-3xl mt-6 mb-4">Sign Up</h2>
             <div className="grid md:grid-cols-2 font-GraphikBlack w-[80%] gap-3 ">
               <div className="relative z-0 w-full mb-3 md:mb-6 group">
-                <input {...register("firstName")} type="text" id="firstName" name="firstName"
-                  className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-[#0FADFF] peer" placeholder=" " required />
+                <Input {...register('firstName')} name="firstName" id="firstName" />
+
                 <label htmlFor="firstName" className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#0FADFF] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">First name</label>
               </div>
               <div className="relative z-0 w-full mb-3 md:mb-6 group">
-                <input {...register("lastName")} type="text" name="lastName" id="lastName"
-                  className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-[#0FADFF] peer" placeholder=" " required />
+                <Input {...register('lastName')} name="lastName" id="lastName" />
+
                 <label htmlFor="lastName" className="peer-focus:font-medium absolute text-sm text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#0FADFF] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Last name</label>
               </div>
             </div>
             <div className="relative z-0 w-[80%] mb-5 group font-GraphikBlack">
-              <input {...register("email")} type="text" name="email"
-                className="block py-2.5 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-[#0FADFF] focus:outline-none focus:ring-0 focus:border-[#0FADFF] peer" placeholder=" " required />
+              <Input {...register('email')} name="email" id="email" type="email" />
+
               <label htmlFor="email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-[#0FADFF] peer-focus:dark:text-[#0FADFF] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email</label>
             </div>
             <div className="relative z-0 w-[80%] mb-5 group font-GraphikBlack">
-              <input {...register("password")} type="password" name="password" id="password"
-                className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-[#0FADFF] focus:outline-none focus:ring-0 focus:border-[#0FADFF] peer" placeholder=" " required />
+              <Input {...register("password")} type="password" name="password" id="password" />
+
               <label htmlFor="password" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-[#0FADFF] peer-focus:dark:text-[#0FADFF] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
             </div>
             <button disabled={isSubmitting} className="bg-[#0FADFF] mt-2 text-white px-16 rounded-full py-2 font-GraphikBlack">
