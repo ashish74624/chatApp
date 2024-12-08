69c112d4923e755e9ad38efa7495ce8a18027ef7
Ashish <ashish74624@gmail.com>
2024-12-08 23:19:02 +0530

    route

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,12 @@
 import express from "express";
 import { login, register } from "../controller/userController";
+import { verify } from "../middleware/AuthMiddleware";
 
 const router = express.Router();
 
 router.post("/login",login);
 router.post("/signup",register);
+router.post("/",verify);
+
 
 export default router;
